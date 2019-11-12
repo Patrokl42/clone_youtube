9296ae8392f4c5b82468d9495852e0a9b9550637
Petro <petro.golovetsky@chaosbyte.io>
2019-11-12 08:45:46 +0200

    controllers for video

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,12 +1,15 @@
 const videoRouter = require('express').Router();
 const routes = require('../routes');
 const videoController = require('../controllers/videoController');
+const middlewares = require('../middlewares');
 
 videoRouter.get(routes.uploadVideo, videoController.getUpload);
-videoRouter.post(routes.uploadVideo, videoController.postUpload);
-
+videoRouter.post(routes.uploadVideo, middlewares.uploadVideo, videoController.postUpload);
 videoRouter.get(routes.videoDetail(), videoController.detail);
-videoRouter.get(routes.editVideo, videoController.edit);
-videoRouter.get(routes.deleteVideo, videoController.videoDelete);
+
+videoRouter.get(routes.editVideo(), videoController.getEdit);
+videoRouter.post(routes.editVideo(), videoController.postEdit);
+
+videoRouter.get(routes.deleteVideo(), videoController.videoDelete);
 
 module.exports = videoRouter;
